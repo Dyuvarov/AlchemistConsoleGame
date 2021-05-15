c3dd21069575850aa2dc82244f4a5543a78a7d1b
Dyuvarov <A.Duvarovv@yandex.ru>
2021-05-15 17:05:53 +0300

    create test class

---
+++ src/test/java/com/game/InventoryTest.java
@@ -0,0 +1,17 @@
+package com.game;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InventoryTest {
+//    add different inventories as fields
+    public InventoryTest()
+    {
+        //initialize  inventories
+    }
+
+    public void findTestNullString() throws Exception {
+
+    }
+}
